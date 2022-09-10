7b95dd94699a6995ce12c411ef35696516ce05a0
Ozone <owenzonneveld@gmail.com>
2022-09-10 15:25:23 -0500

    Updated Version number

---
+++ target/debug/deps/text_analyzer-40e10d8316358198.d
@@ -0,0 +1,5 @@
+c:\Users\Ozone\Programming\Rust Projects\text_analyzer\target\debug\deps\text_analyzer-40e10d8316358198.rmeta: src\main.rs
+
+c:\Users\Ozone\Programming\Rust Projects\text_analyzer\target\debug\deps\text_analyzer-40e10d8316358198.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/text_analyzer-94d7537b2afdf8cb.d
@@ -0,0 +1,5 @@
+c:\Users\Ozone\Programming\Rust Projects\text_analyzer\target\debug\deps\text_analyzer-94d7537b2afdf8cb.rmeta: src\main.rs
+
+c:\Users\Ozone\Programming\Rust Projects\text_analyzer\target\debug\deps\text_analyzer-94d7537b2afdf8cb.d: src\main.rs
+
+src\main.rs:
